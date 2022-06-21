4e3bbf546adbbe1680f727e8cbb9db6f86fd2b62
cruelladevil <dev.timetravel@gmail.com>
2022-06-21 23:35:33 +0900

    Add arranging-coins

---
+++ src/arranging-coins/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function (n) {
+  let left = 1;
+  let right = n;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    const sum = ((1 + middle) * middle) / 2;
+
+    if (sum === n) return middle;
+
+    if (sum < n) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+
+  return left - 1;
+};
