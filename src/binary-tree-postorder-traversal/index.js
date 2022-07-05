1477fde5a86f83141b24c33d06aacbb895453694
cruelladevil <dev.timetravel@gmail.com>
2022-07-05 16:49:15 +0900

    Add binary-tree-postorder-traversal

---
+++ src/binary-tree-postorder-traversal/index.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// Recursive
+var postorderTraversalRecursive = function (root) {
+  const result = [];
+
+  const traverse = (node) => {
+    if (node === null) return;
+
+    traverse(node.left);
+    traverse(node.right);
+    result.push(node.val);
+  };
+
+  traverse(root);
+
+  return result;
+};
+
+// Stack
+var postorderTraversalStack = function (root) {
+  if (root === null) return [];
+
+  const result = [];
+  const stack = [root];
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+    result.push(node.val);
+
+    if (node.left !== null) {
+      stack.push(node.left);
+    }
+    if (node.right !== null) {
+      stack.push(node.right);
+    }
+  }
+
+  return result;
+};
