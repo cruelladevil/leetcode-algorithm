816c1389554da5ace22cab47b58e3f8ec5b3fd95
cruelladevil <dev.timetravel@gmail.com>
2022-07-13 23:07:19 +0900

    Add n-ary-tree-preorder-traversal

---
+++ src/n-ary-tree-preorder-traversal/index.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root) {
+  const result = [];
+
+  const traverse = (node) => {
+    if (node === null) return;
+
+    result.push(node.val);
+
+    node.children.forEach((child) => {
+      traverse(child);
+    });
+  };
+
+  traverse(root);
+
+  return result;
+};
