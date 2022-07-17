84a690d5707f533c56e2a7793d837c34b43497bb
cruelladevil <dev.timetravel@gmail.com>
2022-07-17 23:36:35 +0900

    Update flood-fill

--- src/flood-fill/index.js
+++ src/flood-fill/index.js
@@ -2,18 +2,20 @@
  * @param {number[][]} image
  * @param {number} sr
  * @param {number} sc
- * @param {number} newColor
+ * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function (image, sr, sc, newColor) {
+var floodFill = function (image, sr, sc, color) {
+  const m = image.length;
+  const n = image[0].length;
   const targetColor = image[sr][sc];
 
   const fillImageFrom = function fill(row, col) {
-    const isOutOfImage = row < 0 || col < 0 || row >= image.length || col >= image[0].length;
+    const isOutOfImage = row < 0 || col < 0 || row >= m || col >= n;
 
-    if (isOutOfImage || image[row][col] === newColor || image[row][col] !== targetColor) return;
+    if (isOutOfImage || image[row][col] !== targetColor || image[row][col] === color) return;
 
-    image[row][col] = newColor;
+    image[row][col] = color;
 
     fill(row - 1, col);
     fill(row + 1, col);
