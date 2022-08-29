66add06b615402d3cab1d688b7440d410c78e1cf
cruelladevil <dev.timetravel@gmail.com>
2022-08-29 23:34:27 +0900

    Add sign-of-the-product-of-an-array

---
+++ src/sign-of-the-product-of-an-array/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function (nums) {
+  let sign = 1;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    const num = nums[i];
+
+    if (num === 0) {
+      return 0;
+    }
+
+    if (num < 0) {
+      sign *= -1;
+    }
+  }
+
+  return sign;
+};
