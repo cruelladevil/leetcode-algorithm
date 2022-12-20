5f28671de792d2ad3ec362405adbdc2e18262ae5
cruelladevil <dev.timetravel@gmail.com>
2022-12-20 20:28:55 +0900

    Update maximum-subarray

--- src/maximum-subarray/index.js
+++ src/maximum-subarray/index.js
@@ -4,7 +4,7 @@
  */
 var maxSubArray = function (nums) {
   let curSum = 0;
-  let maxSum = nums[0];
+  let maxSum = -Infinity;
 
   nums.forEach((num) => {
     curSum = Math.max(curSum + num, num);
