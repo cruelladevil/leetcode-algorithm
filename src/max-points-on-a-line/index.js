3fc9654450a81ce4cd8876b68fe6c5bad81138b9
cruelladevil <dev.timetravel@gmail.com>
2023-01-09 20:12:12 +0900

    Update max-points-on-a-line

--- src/max-points-on-a-line/index.js
+++ src/max-points-on-a-line/index.js
@@ -5,32 +5,26 @@
 var maxPoints = function (points) {
   let max = 1;
 
+  const getSlope = (pointA, pointB) => {
+    const [x1, y1] = pointA;
+    const [x2, y2] = pointB;
+    const slope = (y2 - y1) / (x2 - x1);
+
+    return Math.abs(slope) === Infinity ? 'vertical' : slope;
+  };
+
   for (let i = 0; i < points.length; i += 1) {
-    const [x1, y1] = points[i];
+    const pointA = points[i];
     const pointMap = {};
-    let samePointCount = 0;
 
     for (let j = i + 1; j < points.length; j += 1) {
-      const [x2, y2] = points[j];
-
-      if (x1 === x2 && y1 === y2) {
-        samePointCount += 1;
-      } else {
-        let slope = (y2 - y1) / (x2 - x1);
-
-        if (!Number.isFinite(slope)) {
-          slope = 'vertical';
-        } else if (Number.isNaN(slope)) {
-          slope = 'horizontal';
-        }
-
-        pointMap[slope] = (pointMap[slope] ?? 1) + 1;
-      }
+      const pointB = points[j];
+      const slope = getSlope(pointA, pointB);
+      pointMap[slope] = (pointMap[slope] ?? 1) + 1;
     }
 
     const maxPointCount = Math.max(...Object.values(pointMap));
-
-    max = Math.max(maxPointCount + samePointCount, max);
+    max = Math.max(maxPointCount, max);
   }
 
   return max;
