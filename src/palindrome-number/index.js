2c3f42acb5d751617697a06ff7f9b32e1aa3ffc7
cruelladevil <dev.timetravel@gmail.com>
2023-01-23 14:13:54 +0900

    Update palindrome-number

--- src/palindrome-number/index.js
+++ src/palindrome-number/index.js
@@ -2,13 +2,35 @@
  * @param {number} x
  * @return {boolean}
  */
+// String
 var isPalindrome = function (x) {
-  const string = x.toString();
-  const { length } = string;
+  const numString = x.toString();
+  let left = 0;
+  let right = numString.length - 1;
 
-  for (let i = 0; i < length / 2; i += 1) {
-    if (string[i] !== string[length - 1 - i]) return false;
+  while (left <= right) {
+    if (numString.charAt(left) !== numString.charAt(right)) {
+      return false;
+    }
+
+    left += 1;
+    right -= 1;
   }
 
   return true;
+};
+
+// Number
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+
+  let num = x;
+  let reverse = 0;
+
+  while (num > 0) {
+    reverse = reverse * 10 + (num % 10);
+    num = Math.floor(num / 10);
+  }
+
+  return x === reverse;
 };
