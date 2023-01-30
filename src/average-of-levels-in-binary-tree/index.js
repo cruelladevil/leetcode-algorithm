28007071783cd80c0c4ddff7b43536560ca0de82
cruelladevil <dev.timetravel@gmail.com>
2023-01-30 20:50:09 +0900

    Update average-of-levels-in-binary-tree

--- src/average-of-levels-in-binary-tree/index.js
+++ src/average-of-levels-in-binary-tree/index.js
@@ -10,24 +10,46 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// dfs
 var averageOfLevels = function (root) {
   const levelSums = [];
 
-  const traverse = (node, level) => {
+  const dfs = (node, level = 0) => {
     if (node === null) return;
 
-    if (!levelSums[level]) {
-      levelSums[level] = { count: 0, sum: 0 };
-    }
+    levelSums[level] ??= { sum: 0, count: 0 };
 
-    levelSums[level].count += 1;
     levelSums[level].sum += node.val;
+    levelSums[level].count += 1;
 
-    traverse(node.left, level + 1);
-    traverse(node.right, level + 1);
+    dfs(node.left, level + 1);
+    dfs(node.right, level + 1);
   };
 
-  traverse(root, 0);
+  dfs(root);
 
   return levelSums.map(({ sum, count }) => sum / count);
+};
+
+// bfs
+var averageOfLevels = function (root) {
+  const everages = [];
+  const queue = new Queue([root]);
+
+  while (!queue.isEmpty()) {
+    const size = queue.size();
+    let sum = 0;
+
+    for (let i = 0; i < size; i += 1) {
+      const node = queue.dequeue();
+      sum += node.val;
+
+      if (node.left !== null) queue.enqueue(node.left);
+      if (node.right !== null) queue.enqueue(node.right);
+    }
+
+    everages.push(sum / size);
+  }
+
+  return everages;
 };
