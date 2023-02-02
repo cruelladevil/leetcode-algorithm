955bea7177abb06491d7aa55d5eddce7a7548421
cruelladevil <dev.timetravel@gmail.com>
2023-02-02 18:35:59 +0900

    Update verifying-an-alien-dictionary

--- src/verifying-an-alien-dictionary/index.js
+++ src/verifying-an-alien-dictionary/index.js
@@ -4,28 +4,41 @@
  * @return {boolean}
  */
 var isAlienSorted = function (words, order) {
-  const orderMap = {};
+  const makeOrderMap = (orderString) => {
+    const n = orderString.length;
+    const orderMap = {};
 
-  for (let i = 0; i < order.length; i += 1) {
-    const char = order.charAt(i);
-    orderMap[char] = i;
-  }
+    for (let i = 0; i < n; i += 1) {
+      const char = order.charAt(i);
+      orderMap[char] = i;
+    }
 
-  for (let i = 0; i < words.length - 1; i += 1) {
-    const curr = words[i];
-    const next = words[i + 1];
+    return orderMap;
+  };
 
-    for (let j = 0; j < curr.length; j += 1) {
-      if (j >= next.length) return false;
+  const orderMap = makeOrderMap(order);
 
-      const currChar = curr.charAt(j);
-      const nextChar = next.charAt(j);
+  const orderCompare = (s1, s2) => {
+    const lenS1 = s1.length;
+    const lenS2 = s2.length;
 
-      if (orderMap[currChar] > orderMap[nextChar]) {
-        return false;
-      } else if (orderMap[currChar] < orderMap[nextChar]) {
-        break;
+    for (let i = 0; i < lenS1 && i < lenS2; i += 1) {
+      const charS1 = s1.charAt(i);
+      const charS2 = s2.charAt(i);
+
+      if (charS1 !== charS2) {
+        return orderMap[charS1] - orderMap[charS2];
       }
+    }
+
+    return lenS1 - lenS2;
+  };
+
+  const n = words.length;
+
+  for (let i = 0; i < n - 1; i += 1) {
+    if (orderCompare(words[i], words[i + 1]) > 0) {
+      return false;
     }
   }
 
